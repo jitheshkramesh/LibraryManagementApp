6373adca642f87443706efe9c76deade8d3a62d0
Jithesh K Ramesh <31313065+jitheshkramesh@users.noreply.github.com>
2019-06-12 10:21:44 +0400

    Add files via upload

---
+++ ViewModel/CreateOuterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LibraryManagementApp.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagementApp.ViewModel
+{
+    public class CreateOuterViewModel
+    {
+        public Author Author { get; set; }
+        public string Referer  { get; set; }
+    }
+}
